30c8d6b8e81572fbebd2dd0e61cb9e883f6eada5
Deyan Svetoslavov <dsvetoslavov@gmail.com>
2025-08-11 21:12:46 +0300

    Fix completion plugin

--- nvim/.config/nvim/lua/config/completion.lua
+++ nvim/.config/nvim/lua/config/completion.lua
@@ -1,15 +1,37 @@
 local cmp = require("cmp")
 
+local ok_snip, luasnip = pcall(require, "luasnip")
+if ok_snip then
+  require("luasnip.loaders.from_vscode").lazy_load()
+  luasnip.config.set_config({
+    history = true,
+    updateevents = "TextChanged,TextChangedI",
+  })
+end
+
 cmp.setup({
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      if ok_snip then luasnip.lsp_expand(args.body) end
     end,
   },
   mapping = cmp.mapping.preset.insert({
     ["<Tab>"] = cmp.mapping.select_next_item(),
     ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+    ["<CR>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        local entry = cmp.get_selected_entry()
+        if not entry then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        end
+        -- INSERT behavior avoids “clears and jumps” glitches
+        cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+        return
+      end
+      fallback()
+    end, { "i", "s" }),
+
     ["<C-Space>"] = cmp.mapping.complete(),
   }),
   sources = cmp.config.sources({
@@ -20,3 +42,8 @@ cmp.setup({
   }),
 })
 
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+local ok_pairs, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+if ok_pairs then
+  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+end
--- nvim/.config/nvim/lua/config/plugins.lua
+++ nvim/.config/nvim/lua/config/plugins.lua
@@ -48,6 +48,19 @@ require("lazy").setup({
   { "hrsh7th/cmp-nvim-lsp" },
   { "hrsh7th/cmp-buffer" },
   { "hrsh7th/cmp-path" },
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      "windwp/nvim-autopairs",
+    },
+  },
 })
 
 -- Theme setup
